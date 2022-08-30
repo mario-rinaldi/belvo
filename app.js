b22034a16f4f3d8fe5dc7028bca696c9c819accd
mario-rinaldi <mlealrinaldi@gmail.com>
2022-08-30 02:10:13 -0300

    fixed bug with page redirect

--- app.js
+++ app.js
@@ -45,11 +45,11 @@ app.get("/token", (req, res) => {
     })
 });
 
-let response = null;
+let response;
 
 io.on("connection", (socket) => {
     socket.on('callbackBelvo', (data) => {
-        response = JSON.parse(data)
+        response = data
         socket.emit('redirect', '/links/' + response.link)
     });
     socket.on('successPageLoad', (data) => {
@@ -57,6 +57,63 @@ io.on("connection", (socket) => {
     });
   });
 
+/*
+const env = {
+    secretId: '4cdd734e-b289-4303-b2af-31db6530c82f',
+    secretPassword: 'Ld6ERSqTbyczS22WsdpKgoFopoykPiJoODM*arEgS@BG2lN*vHz58QRkqPCb4OxA',
+    baseUrl: 'https://sandbox.belvo.com',
+    authorization: 'Basic NGNkZDczNGUtYjI4OS00MzAzLWIyYWYtMzFkYjY1MzBjODJmOkxkNkVSU3FUYnljelMyMldzZHBLZ29Gb3BveWtQaUpvT0RNKmFyRWdTQEJHMmxOKnZIejU4UVJrcVBDYjRPeEE='
+}
+
+function fetchBelvoAPI() {
+    //call owners endpoint
+    console.log('ID STRING ' + response.link.toString())
+
+    let ownersEndpoint = env.baseUrl + '/api/owners/?page=1&link=047685ce-2de2-4639-bc20-960364c31b08' + response.link
+    console.log(ownersEndpoint)
+    fetch(encodeURI(ownersEndpoint), {
+        headers: { 'Authorization': env.authorization }
+    })
+    .then(res => res.json())
+    .then(json => {
+        console.log("Owners: " + JSON.stringify(json));
+    });
+
+    //call accounts endpoint
+    let accountsEndpoint = env.baseUrl + '/api/accounts/?page=1&link=' + response.link
+    console.log(accountsEndpoint)
+    fetch(encodeURI(accountsEndpoint), {
+        headers: { 'Authorization': env.authorization }
+    })
+    .then(res => res.json())
+    .then(json => {
+        console.log("Accounts: " + JSON.stringify(json));
+    })
+
+    //call transactions endpoint
+    let transactionsEndpoint = env.baseUrl + '/api/transactions/?page=1&link=' + response.link.toString()
+    console.log(transactionsEndpoint)
+    fetch(encodeURI(transactionsEndpoint), {
+        headers: { 'Authorization': env.authorization }
+    })
+    .then(res => res.json())
+    .then(json => {
+        console.log("Transactions: " + JSON.stringify(json));
+    })
+
+    //call incomes endpoint
+    let incomesEndpoint = env.baseUrl + '/api/incomes/?page=1&account=' + response.link //aqui é o income de uma ACCOUNT ID
+    console.log(incomesEndpoint)
+    fetch(encodeURI(incomesEndpoint), {
+        headers: { 'Authorization': env.authorization }
+    })
+    .then(res => res.json())
+    .then(json => {
+        console.log("Incomes: " + JSON.stringify(json));
+    })
+
+}
+*/
 const userRouter = require('./routes/users')
 app.use('/users', userRouter)
 
--- routes/links.js
+++ routes/links.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const app = express();
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const fetch = require('node-fetch');
 
 router.get('/', (req, res) => {
     res.render('success')
@@ -11,20 +15,79 @@ router
         res.render('success')
         //res.send(`Get User With Id ${req.params.linkId}`)
     })
-    .put((req, res) => {
-        res.send(`Update User With Id ${req.params.linkId}`)
-    })
-    .delete((req, res) => {
-        res.send(`Delete User With Id ${req.params.linkId}`)
-    })
 
-const users = [{name: "Mario"}, {name: "Sara"}]
+const env = {
+    secretId: '4cdd734e-b289-4303-b2af-31db6530c82f',
+    secretPassword: 'Ld6ERSqTbyczS22WsdpKgoFopoykPiJoODM*arEgS@BG2lN*vHz58QRkqPCb4OxA',
+    baseUrl: 'https://sandbox.belvo.com',
+    authorization: 'Basic NGNkZDczNGUtYjI4OS00MzAzLWIyYWYtMzFkYjY1MzBjODJmOkxkNkVSU3FUYnljelMyMldzZHBLZ29Gb3BveWtQaUpvT0RNKmFyRWdTQEJHMmxOKnZIejU4UVJrcVBDYjRPeEE='
+}
 
-router.param("id", (req, res, next, id) => {
-    req.user = users[id]
+router.param("linkId", (req, res, next, linkId) => {
+
+        //call owners endpoint
+        let ownersEndpoint = env.baseUrl + '/api/owners/?page=1&link=' + req.params.linkId
+        console.log(ownersEndpoint)
+        fetch(ownersEndpoint, {
+            headers: { 'Authorization': env.authorization }
+        })
+        .then(res => res.json())
+        .then(json => {
+            console.log("Owners: " + JSON.stringify(json));
+        });
+
     next()
 })
 
 
+
+/*
+function fetchBelvoAPI() {
+    //call owners endpoint
+    let ownersEndpoint = env.baseUrl + '/api/owners/?page=1&link=047685ce-2de2-4639-bc20-960364c31b08' + response.link
+    console.log(ownersEndpoint)
+    fetch(encodeURI(ownersEndpoint), {
+        headers: { 'Authorization': env.authorization }
+    })
+    .then(res => res.json())
+    .then(json => {
+        console.log("Owners: " + JSON.stringify(json));
+    });
+
+    //call accounts endpoint
+    let accountsEndpoint = env.baseUrl + '/api/accounts/?page=1&link=' + response.link
+    console.log(accountsEndpoint)
+    fetch(encodeURI(accountsEndpoint), {
+        headers: { 'Authorization': env.authorization }
+    })
+    .then(res => res.json())
+    .then(json => {
+        console.log("Accounts: " + JSON.stringify(json));
+    })
+
+    //call transactions endpoint
+    let transactionsEndpoint = env.baseUrl + '/api/transactions/?page=1&link=' + response.link.toString()
+    console.log(transactionsEndpoint)
+    fetch(encodeURI(transactionsEndpoint), {
+        headers: { 'Authorization': env.authorization }
+    })
+    .then(res => res.json())
+    .then(json => {
+        console.log("Transactions: " + JSON.stringify(json));
+    })
+
+    //call incomes endpoint
+    let incomesEndpoint = env.baseUrl + '/api/incomes/?page=1&account=' + response.link //aqui é o income de uma ACCOUNT ID
+    console.log(incomesEndpoint)
+    fetch(encodeURI(incomesEndpoint), {
+        headers: { 'Authorization': env.authorization }
+    })
+    .then(res => res.json())
+    .then(json => {
+        console.log("Incomes: " + JSON.stringify(json));
+    })
+
+}
+*/
 
 module.exports = router
